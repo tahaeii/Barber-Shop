fd91be744e53c3e41d2dd64146b576b0d7cb1878
tahaeii <tahaeii@gmail.com>
2023-11-01 21:30:11 +0330

    SelectedServices End

--- src/app/section4/section4.component.ts
+++ src/app/section4/section4.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 import { Data } from '@angular/router';
 
 @Component({
@@ -8,6 +9,8 @@ import { Data } from '@angular/router';
   styleUrls: ['./section4.component.css']
 })
 export class Section4Component {
+
+  @ViewChild('stepper') stepper !: MatStepper;
 
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
@@ -21,58 +24,65 @@ export class Section4Component {
     secondCtrl: ['', Validators.required],
   });
 
-  selected !: Date | null;
+  fourthFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  // Draft Start ***********************************************************
+
+  // selected !: Date | null;
 
   // Celander
 
-  minDate !: Date;
-  maxDate !: Date;
-  showSelectedDateComponent = false;
+  // minDate !: Date;
+  // maxDate !: Date;
+  // showSelectedDateComponent = false;
 
-  onDateChange(event: any) {
-    this.showSelectedDateComponent = true;
-  }
+  // onDateChange(event: any) {
+  //   this.showSelectedDateComponent = true;
+  // }
 
   constructor(private _formBuilder: FormBuilder) {
-    this.minDate = new Date();
+    // this.minDate = new Date();
 
-    this.maxDate = new Date();
-    this.maxDate.setDate(this.maxDate.getDate() + 10);
+    // this.maxDate = new Date();
+    // this.maxDate.setDate(this.maxDate.getDate() + 10);
 
     // Access To Celander Dates
   }
 
-  Grooming: string = '';
-  otherServicesValue: string = '';
+  // Grooming: string = '';
+  // otherServicesValue: string = '';
 
-  groomingSelect(category: string) {
-    this.Grooming = category;
-    this.showSelectedDateComponent = true;
-  }
+  // groomingSelect(category: string) {
+  //   this.Grooming = category;
+  //   this.showSelectedDateComponent = true;
+  // }
 
-  servicesSelect(category: string) {
-    this.otherServicesValue = category;
-    this.showSelectedDateComponent = true;
-  }
+  // servicesSelect(category: string) {
+  //   this.otherServicesValue = category;
+  //   this.showSelectedDateComponent = true;
+  // }
   // Save & Show MenuButton In Input
 
 
-  disableSelect = new FormControl(false);
+  // disableSelect = new FormControl(false);
 
   // Disable Grooming & OtherServices Button & Input
 
-  shwCmpnnt(event: any) {
-    if (event.checked) {
-      this.showSelectedDateComponent = true;
-    } else {
-      this.showSelectedDateComponent = false;
-    }
-  }
+  // shwCmpnnt(event: any) {
+  //   if (event.checked) {
+  //     this.showSelectedDateComponent = true;
+  //   } else {
+  //     this.showSelectedDateComponent = false;
+  //   }
+  // }
 
   // Disable Grooming & OtherServices Button & Input When Radio
   // Button Will Be True !
 
-
+  // Draft End ***********************************************************
+  // Draft was for old design !
   // Again Start ..
 
   services = [
@@ -146,28 +156,43 @@ export class Section4Component {
   isSelected !: boolean;
   selectedTitles: string[] = [];
   visibleServices: number = 6;
+  diasableButton = true;
+  showSpinner = false;
+  loadDate: boolean = false;
 
   selectedService(service: any) {
     service.isSelected = !service.isSelected;
     if (service.isSelected) {
-      this.selectedTitles.push(service.title);
+      this.selectedTitles.push(service.id);
     } else {
       this.selectedTitles = this.selectedTitles.filter(id => id !== service.id);
     }
-  }
-  // Select a service & save id
+    this.diasableButton = this.selectedTitles.length <= 0;
+    // For disable & anable button when user dont select servise
+  }   // Select a service & save id
+
+  loadMore() {
+    this.visibleServices += 2;
+  } // Load more button
 
   showSelectedTitles() {
     this.services.forEach(service => {
       service.isSelected = false;
-    });
+    }); // Unselected Service that user was selected
+    this.showSpinner = true;
+    this.diasableButton = true;
+    this.loadDate = true;
+
+    setTimeout(() => {
+      this.showSpinner = false;
+      this.diasableButton = false;
+      this.stepper.next();
+    }, 3000);
+    // Show Loader
   }
-  // isSelected will false, after click on button
 
-  loadMore() {
-    this.visibleServices += 2;
+  resetService() {
+    this.loadDate = false;
+    this.visibleServices = 6;
   }
-  // Load more button
-
-
 }
